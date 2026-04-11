66c207412b7d5c5d1e2ba4739d1108c40bf55c0f
hdugan <hilarydugan@gmail.com>
2026-04-11 10:23:39 -0500

    Nitrogen data pulls and graph

---
+++ 6_n_habs/1_getUSGS_N.R
@@ -0,0 +1,132 @@
+library(dataRetrieval)
+library(sf)
+library(tidyverse)
+
+# Yahara at 113 annual discharge
+N.y113 <- read_waterdata_daily(
+  monitoring_location_id = "USGS-05427850", 
+  parameter_code = c("00060")) |> 
+  mutate(year = year(time)) |> 
+  # mutate( water_year = if_else(month(time) >= 10, year(time) + 1, year(time))) |> 
+  group_by(year) |> 
+  summarise(annualQ = sum(value * 0.0283168 * 86400))
+mean(N.y113$annualQ)
+
+p <- read_waterdata_monitoring_location("USGS-05427718")
+codes = read_waterdata_combined_meta(monitoring_location_id = "USGS-05427718")
+codes |> dplyr::select(parameter_code, parameter_name) |> arrange(parameter_code) |> distinct()
+
+N.yw <- read_waterdata_daily(
+  monitoring_location_id = "USGS-05427718", 
+  parameter_code = c("00060", "00631", "00625"))  |> 
+  select(gage = monitoring_location_id, parameter_code, time, value) |> 
+  pivot_wider(names_from = parameter_code, values_from = value) |> 
+  st_drop_geometry() |> 
+  mutate(flow_m3s = `00060` * 0.0283168) |> 
+  rename(no3 = `00631`, NH3_orgN = `00625`, date = time)
+
+N.pb <- read_waterdata_daily(
+  monitoring_location_id = "USGS-05427948", 
+  parameter_code = c("00060", "00631", "00625")) |> 
+  select(gage = monitoring_location_id, parameter_code, time, value) |> 
+  pivot_wider(names_from = parameter_code, values_from = value) |> 
+  st_drop_geometry() |> 
+  mutate(flow_m3s = `00060` * 0.0283168) |> 
+  rename(no3 = `00631`, NH3_orgN = `00625`, date = time)
+
+N.2 <- N.pb %>% bind_rows(N.yw) |> 
+  mutate(year = year(date))
+
+valid_years <- N.2 %>%
+  group_by(gage, year) %>%
+  summarise(n_days = sum(!is.na(no3)), .groups = "drop") %>%
+  filter(n_days > 300)
+
+N.2 = N.2 |> semi_join(valid_years, by = c("gage", "year")) |> 
+    # water_year = if_else(month(date) >= 10, year(date) + 1, year(date)),  # Oct–Sep water year
+  mutate(
+    load_no3_kg_d = flow_m3s * no3 * 86400 * 1e-3,
+    load_no4_kg_d = flow_m3s * NH3_orgN * 86400 * 1e-3) %>%
+  group_by(gage, year) %>%
+  filter(sum(!is.na(no3)) > 0) %>%
+  arrange(date) %>%
+  group_by(gage, year) %>%
+  mutate(
+    cumQ = cumsum(flow_m3s * 86400),   # cumulative volume (m3)
+    no3_Load = cumsum(load_no3_kg_d),
+    nh4_orgN_Load = cumsum(load_no4_kg_d)) %>%
+  ungroup()
+
+# write_csv(N.2, 'NitrogenFig/usgsTribN.csv')
+
+ggplot(N.2, aes(x = cumQ, y = nh4_orgN_Load, color = gage)) +
+  geom_path(alpha = 0.8, linewidth = 1) +
+  labs(
+    x = "Cumulative Discharge (m³)",
+    y = "Cumulative NO3 Load (kg)",
+    color = "Water Year") +
+  theme_minimal() +
+  facet_wrap(~gage, scales = 'free')
+
+N.2_yearly <- N.2 %>%
+  group_by(gage, year) %>%
+  summarise(total_Q = max(cumQ, na.rm = TRUE),
+    NO3 = max(no3_Load, na.rm = TRUE),
+    NH4_orgN = max(nh4_orgN_Load, na.rm = TRUE),
+    .groups = "drop") |> 
+  pivot_longer(cols = 4:5) |> 
+  mutate(gage_name = recode(
+      gage,
+      "USGS-05427718" = "Trib: Yahara @ Windsor",
+      "USGS-05427948" = "Trib: Pheasant Branch"))
+
+# Windsor discharge is 4.5x than PB
+N.2_yearly |> group_by(gage) |> 
+  summarise(mean(total_Q))
+6546304 # PB
+30048053 # Windsor
+73963745 # 113
+
+p.tribs = ggplot(N.2_yearly) +
+  geom_point(aes(x = total_Q/1e6, y = value/1e4, fill = name, shape = name), size = 2, alpha = 0.8) +
+  facet_wrap(~ gage_name, scales = "free") +
+  labs(x = expression(Annual~Discharge~(10^6*~m^3)),
+    y = expression(Load~(10^4*~kg))) +
+  scale_shape_manual(values = c(23,22), name = '') +
+  scale_fill_manual(values = c('#7f1147', '#c98c3d'), name = '') +
+  scale_x_continuous(n.breaks = 3) +
+  theme_bw(base_size = 9) +
+  theme(
+    # panel.border = element_blank(),
+    strip.background = element_blank(),
+    strip.text = element_text(color = "black")); p.tribs
+
+p.tribs.l = p.tribs +
+  theme(
+    legend.title =  element_blank(),
+    legend.key.size = unit(12, "pt"),
+    legend.margin = margin(0.5, 3, 0.5, 0.5),
+    legend.position = c(0,1),   # x, y (0–1 coordinates)
+    legend.justification = c(0, 1),    # anchor top-left of legend box
+    legend.background = element_rect(fill = "white", color = "black", linewidth = 0.2)
+  ); p.tribs.l
+#7f1147
+#c98c3d
+
+# N.2_yearly <- N.2_yearly %>%
+#   group_by(gage) %>%
+#   mutate(
+#     Q_rel = total_Q / max(total_Q),
+#     load_rel = total_load / max(total_load)
+#   ) %>%
+#   ungroup()
+# 
+# ggplot(N.2_yearly, aes(Q_rel, load_rel, color = gage)) +
+#   geom_point(size = 2) +
+#   geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
+#   theme_bw(base_size = 9) +
+#   labs(y = 'Normalized annual nitrate load', x = 'Normalized annual discharge', 
+#        caption = 'Water-year cumulative nitrate load (load_rel) versus cumulative discharge (Q_rel), 
+#        normalized to the maximum annual values within each gage. Each point represents a single water 
+#        year, allowing comparison of export efficiency across years and sites.')
+
---
+++ 6_n_habs/2_NO3decline.R
@@ -0,0 +1,58 @@
+library(MetBrewer)
+library(scales)
+library(tidyverse)
+library(patchwork)
+library(cowplot)
+################## Source tributary plot ##################
+source('6_n_habs/1_getUSGS_N.R')
+
+################## load MEMO aphan data ##################
+aphan = read_csv('6_n_habs/aphanizomenon_APHAN134_first_bloom.csv')
+
+################## load NTL nutrient data ##################
+nuts = loadLTERnutrients() |> filter(lakeid == 'ME', depth == 0) |> 
+  filter(!is.na(no3no2_WSLH)) |> 
+  filter(yday(sampledate) >= 100 & yday(sampledate) <= 365) |> 
+  select(sampledate, depth, no3no2_WSLH, nh4_WSLH, totnuf_WSLH)
+
+# Day of first 0 nitrate in surface water of Mendota 
+zerono3 = nuts %>%
+  mutate(year = year(sampledate)) %>%
+  group_by(year) %>%
+  filter(no3no2_WSLH == 0) %>%
+  slice_min(sampledate, n = 1, with_ties = FALSE) %>%
+  ungroup()
+
+zeronh4 = nuts %>%
+  mutate(year = year(sampledate)) %>%
+  group_by(year) %>%
+  filter(nh4_WSLH == 0) %>%
+  slice_min(sampledate, n = 1, with_ties = FALSE) %>%
+  ungroup()
+
+p.no3 = ggplot(zerono3) +
+  geom_path(aes(x = year, y = yday(sampledate)), color = '#c98c3d') +
+  geom_point(aes(x = year, y = yday(sampledate)), shape = 21, fill = '#c98c3d', size = 2) +
+  geom_path(data = aphan, aes(x = year, y = yday(date)),color = '#10adad') +
+  geom_point(data = aphan, aes(x = year, y = yday(date)), shape = 21, fill = '#10adad', size = 2) +
+  annotate("text", x = 2016, y = Inf,
+           label = "date~NO[3]~'hits zero'",
+           parse = TRUE, vjust = 2, size = 2.5, color = '#c98c3d') +
+  annotate("text", x = 2017, y = Inf,
+           label = "Aphanizomenon\nfirst appears",
+           vjust = 5.8, size = 2.5, color = '#10adad') +
+  scale_y_continuous(
+    labels = function(x) format(as.Date(x, origin = "2025-01-01"), "%b")) +
+  labs(y = 'Month') +
+  theme_bw(base_size = 9) +
+  theme(axis.title.x = element_blank()); p.no3
+
+## Cowplot
+plot_grid(p.tribs.l, p.no3,
+  ncol = 2, rel_widths = c(1.5,1),
+  labels = c("a)", "b)"), label_size = 8, label_fontface = "plain",
+  align = "v",      # align vertically
+  axis = "lr"       # align left/right axes
+)
+ggsave(filename = '6_n_habs/2_Nfig_v1.png', width = 6.5, height = 1.8, dpi = 500, bg = 'white')  
+
